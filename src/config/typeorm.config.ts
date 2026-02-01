dc022ccbda026394284f5414fbf22f6cacbb5d5a
Nelson-Arenas <racacaca_@hotmail.com>
2026-01-31 21:43:12 -0300

    Conexion a la BD (Completado)

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,14 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
+
+export const typeOrmConfig = (configService: ConfigService) :TypeOrmModuleOptions => ({
+  type: 'postgres',
+  host: configService.get<string>('DATABASE_HOST') ?? 'localhost',
+    port: parseInt(configService.get<string>('DATABASE_PORT') ?? '5432', 10),
+    username: configService.get<string>('DATABASE_USER') ?? 'postgres',
+    password: configService.get<string>('DATABASE_PASSWORD') ?? 'postgres',
+    database: configService.get<string>('DATABASE_NAME') ?? 'postgres',
+    entities: [__dirname + '/../**/*.entity.{js,ts}'],
+    synchronize: true,
+    ssl: true
+});
