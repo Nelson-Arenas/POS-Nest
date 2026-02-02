f1c245fcfbd5161291072dbe53218a0cf3865fc6
Nelson-Arenas <racacaca_@hotmail.com>
2026-02-01 21:02:48 -0300

    Crear Productos

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,23 @@
-export class CreateProductDto {}
+import { IsNotEmpty, IsNumber, IsString } from "class-validator";
+
+export class CreateProductDto {
+    @IsNotEmpty({ message: 'Nombre no debe estar vacío' })
+    @IsString({ message: 'Nombre debe ser una cadena de texto' })
+    name: string;
+
+    //image?: string;
+    @IsString({ message: 'Descripción debe ser una cadena de texto' })
+    description?: string;
+
+    @IsNumber({maxDecimalPlaces: 2}, { message: 'Precio debe ser un número' })
+    @IsNotEmpty({ message: 'Precio no debe estar vacío' })
+    price: number;
+
+    @IsNumber({ maxDecimalPlaces: 0 }, { message: 'Stock debe ser un número' })
+    @IsNotEmpty({ message: 'Stock no debe estar vacío' })
+    stock: number;
+    
+    @IsNumber({ maxDecimalPlaces: 0 }, { message: 'CategoryId debe ser un número' })
+    @IsNotEmpty({ message: 'CategoryId no debe estar vacío' })
+    categoryId: number;
+}
--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,1 +1,26 @@
-export class Product {}
+import { Category } from 'src/categories/entities/category.entity';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Product {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({type: 'varchar', length: 60})
+    name: string;
+
+    @Column({type: 'varchar', length: 255, nullable: true, default: 'default.svg'})
+    image: string;
+
+    @Column({type: 'text', nullable: true})
+    description: string;
+
+    @Column({type: 'float', default: 0})
+    price: number;
+
+    @Column({type: 'int', default: 0})
+    stock: number;
+
+    @ManyToOne(() => Category, (category) => category.products, {eager: true} )
+    category: Category;
+}
